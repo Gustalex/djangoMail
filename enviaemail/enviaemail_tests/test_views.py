f33717c245b9c2aeed302ad45fe48b7d5b42d1de
Gustalex <alexandregustavo00@gmail.com>
2024-05-20 15:51:03 -0300

    feat: adiciona o teste para a função de view envia_email

---
+++ enviaemail/enviaemail_tests/test_views.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, RequestFactory
+from django.core import mail
+from enviaemail.views import envia_email
+import json
+
+class EmailTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_envia_email(self):
+        request = self.factory.post('http://127.0.0.1:8000/email/',
+                                    {'subject': 'Teste Subject',
+                                    'message': 'Teste Message',
+                                    'recipient': 'alexandregustavo00@gmail.com'},
+                                    content_type  = 'application/json')
+        response = envia_email(request)
+        self.assertEqual(response.status_code, 200)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content, {'status': 'success', 'message': 'Email enviado com sucesso'})
+
+        #checa se o email foi enviado
+        self.assertEqual(len(mail.outbox),1)
+        self.assertEqual(mail.outbox[0].subject, 'Teste Subject')
